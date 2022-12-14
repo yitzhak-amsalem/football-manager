7c516eb32b24c3e128175fcc3ef60670c65575e7
Yitzhak Amsalem <yitzhak@gmail.com>
2022-12-14 13:40:29 +0200

    Initial commit

---
+++ src/main/java/com/dev/objects/UserObject.java
@@ -0,0 +1,46 @@
+package com.dev.objects;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserObject {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    public int id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String token;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public void addPost (String post) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
